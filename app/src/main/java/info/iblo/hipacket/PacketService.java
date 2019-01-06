640ee9dc55f2159355444e4e596907e8cbfe704d
Qiu <qiuzeyao@b000000137336.lan>
2019-01-06 21:26:13 +0800

    first commit.

---
+++ app/src/main/java/info/iblo/hipacket/PacketService.java
@@ -0,0 +1,134 @@
+package info.iblo.hipacket;
+
+import android.accessibilityservice.AccessibilityService;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+public class PacketService extends AccessibilityService {
+    private final static String LOG_TAG = "hipacket";
+    private boolean returnFlag = false;
+    private int last_node = 0;
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.d(LOG_TAG,"-------------------------------------------------------------");
+        int eventType = event.getEventType();
+        Log.d(LOG_TAG,"packageName:" + event.getPackageName());
+        Log.d(LOG_TAG,"source:" + event.getSource());
+        Log.d(LOG_TAG,"source class:" + event.getClassName());
+        Log.d(LOG_TAG,"event type(int):" + eventType);
+        AccessibilityNodeInfo root = getRootInActiveWindow();
+        switch (eventType) {
+
+
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                if (returnFlag) {
+                    performBack();
+                    returnFlag = false;
+                    break;
+                }
+                getLastPacket();
+                break;
+
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                String className = event.getClassName().toString();
+
+                if (className.equals("com.baidu.hi.activities.Chat")) {
+                    Log.i(LOG_TAG,"Detected Chat activity view, try to open envelope");
+                    getLastPacket();
+                } else if (className.equals("com.baidu.hi.luckymoney.LuckyMoneyActivity")) {
+                    Log.i(LOG_TAG,"Detected LuckyMoney activity.");
+                    List<AccessibilityNodeInfo> list =
+                        root.findAccessibilityNodeInfosByText("查看我的红包记录");
+                    if (list.size() == 0) {
+                        Log.i(LOG_TAG,"open envelope");
+                        list = root.findAccessibilityNodeInfosByText("拆红包");
+                        AccessibilityNodeInfo parrent = null;
+                        if (list.size() > 0)
+                            parrent = list.get(0);
+                        while (parrent != null) {
+                            if (parrent.isClickable()) {
+                                parrent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                                returnFlag = true;
+                                break;
+                            } else {
+                                parrent = parrent.getParent();
+                            }
+                        }
+                    } else {
+                        Log.i(LOG_TAG,"Back");
+                        performBack();
+                    }
+                }
+                break;
+
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+
+    private void performBack() {
+        //clickAllViewId("com.baidu.hi:id/open_envelope_close_multi");
+        performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
+    }
+
+    private void clickAllViewId(String viewId) {
+        AccessibilityNodeInfo root = getRootInActiveWindow();
+        List<AccessibilityNodeInfo> list =
+                root.findAccessibilityNodeInfosByViewId(viewId);
+        for (AccessibilityNodeInfo node : list) {
+            Log.d(LOG_TAG, node.toString());
+            node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
+    }
+
+    private void clickAllText(String text) {
+        AccessibilityNodeInfo root = getRootInActiveWindow();
+        List<AccessibilityNodeInfo> list =
+                root.findAccessibilityNodeInfosByText(text);
+        for (AccessibilityNodeInfo node : list) {
+            node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
+    }
+
+    private void getLastPacket() {
+        AccessibilityNodeInfo root = getRootInActiveWindow(), node = null;
+        int last = 0;
+        if (root != null) {
+            List<AccessibilityNodeInfo> list =
+                    root.findAccessibilityNodeInfosByViewId("com.baidu.hi:id/chat_item_right_lucky_money_content");
+
+            /*
+            for (AccessibilityNodeInfo item : list) {
+                item.getBoundsInScreen(loc);
+                if (loc.bottom > last) {
+                    last = loc.bottom;
+                    node = item;
+                }
+            }*/
+
+            if (list.size() > 0)
+                node = list.get(list.size() - 1);
+
+            if (node != null) {
+                Rect loc = new Rect();
+                node.getBoundsInScreen(loc);
+                if (last_node != loc.bottom) {
+                    last_node = loc.bottom;
+                    node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                }
+            }
+        }
+    }
+
+
+
+}
